4096b84a4f5a39f6e4a7e2c58f61f10872374ba6
hssh18121 <hssh18121@gmail.com>
2023-12-27 13:07:45 +0700

    Add search logic

--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -1,7 +1,12 @@
-import { findById } from "./BaseService";
+import { findById, update } from "./BaseService";
 
 export async function getSearchHistoryByUserId(userId) {
     const userData = await findById('users', userId);
     return userData.searchHistory;
 }
 
+export async function addDataToSearchHistory(userId, newSearch) {
+    const userData = await findById('users', userId)
+    userData.searchHistory.push(newSearch.trim())
+    update("users", userId, { searchHistory: userData.searchHistory })
+}
