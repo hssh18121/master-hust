2713a5454c413a736874c13a042bc4ee24a9dffe
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2023-12-27 22:47:13 +0700

    hot fix detail post

--- src/components/DetailPost/Comment.jsx
+++ src/components/DetailPost/Comment.jsx
@@ -5,7 +5,12 @@ import { FiChevronsUp, FiChevronsDown } from "react-icons/fi";
 import LikeDislike from "./LikeDislike";
 import SubComment from "./SubComment";
 import CommentForm from "./CommentForm";
-import { getNumberOfDislikesByCommentId, getNumberOfLikesByCommentId, likeOrDislikeComment, unlikeOrUndislikeComment } from "../../services/LikeService";
+import {
+  getNumberOfDislikesByCommentId,
+  getNumberOfLikesByCommentId,
+  likeOrDislikeComment,
+  unlikeOrUndislikeComment,
+} from "../../services/LikeService";
 import { useStateValue } from "../../context/StateProvider";
 
 const Comment = ({ comment, postingComment, setPostingComment }) => {
@@ -13,45 +18,47 @@ const Comment = ({ comment, postingComment, setPostingComment }) => {
   const [newReply, setNewReply] = useState(false);
   const [like, setLike] = useState(0);
   const [dislike, setDislike] = useState(0);
-  const [{ likedOrDislikedComments, userId }] = useStateValue()
-  const [isLiked, setIsLiked] = useState(false)
-  const [isDisliked, setIsDisliked] = useState(false)
+  const [{ likedOrDislikedComments, userId }] = useStateValue();
+  const [isLiked, setIsLiked] = useState(false);
+  const [isDisliked, setIsDisliked] = useState(false);
 
   function checkLikeStatus() {
-    console.log(likedOrDislikedComments)
-    likedOrDislikedComments.forEach(likeOrDislikedComment => {
-      if(likeOrDislikedComment.commentId == comment.id && likeOrDislikedComment.userId == userId) {
-        if(likeOrDislikedComment.likeStatus === true) {
-          console.log(`comment with id ${comment.id} has user like it`)
-          setIsLiked(true)
-        }
-        else {
-          console.log(`comment with id ${comment.id} has user dislike it`)
-          setIsDisliked(true)
+    console.log(likedOrDislikedComments);
+    likedOrDislikedComments.forEach((likeOrDislikedComment) => {
+      if (
+        likeOrDislikedComment.commentId == comment.id &&
+        likeOrDislikedComment.userId == userId
+      ) {
+        if (likeOrDislikedComment.likeStatus === true) {
+          console.log(`comment with id ${comment.id} has user like it`);
+          setIsLiked(true);
+        } else {
+          console.log(`comment with id ${comment.id} has user dislike it`);
+          setIsDisliked(true);
         }
       }
-    })
+    });
   }
 
   async function getLikeNumberForComment() {
-    return await getNumberOfLikesByCommentId(comment.id)
+    return await getNumberOfLikesByCommentId(comment.id);
   }
 
   async function getDislikeNumberForComment() {
-    return await getNumberOfDislikesByCommentId(comment.id)
+    return await getNumberOfDislikesByCommentId(comment.id);
   }
 
   const handleLike = (newIsLiked) => {
     if (newIsLiked) {
-      likeOrDislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, true)
+      likeOrDislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, true);
       setLike(like + 1);
       if (isDisliked) {
         setDislike(dislike - 1);
-        unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, false)
+        unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, false);
         setIsDisliked(false);
       }
     } else {
-      unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, true)
+      unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, true);
       setLike(like - 1);
     }
     setIsLiked(newIsLiked);
@@ -59,15 +66,15 @@ const Comment = ({ comment, postingComment, setPostingComment }) => {
 
   const handleDislike = (newIsDisliked) => {
     if (newIsDisliked) {
-      likeOrDislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, false)
+      likeOrDislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, false);
       setDislike(dislike + 1);
       if (isLiked) {
         setLike(like - 1);
-        unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, true)
+        unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, true);
         setIsLiked(false);
       }
     } else {
-      unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, false)
+      unlikeOrUndislikeComment("7begC0zuZY0c8Qd2GIRm", comment.id, false);
       setDislike(dislike - 1);
     }
     setIsDisliked(newIsDisliked);
@@ -75,13 +82,13 @@ const Comment = ({ comment, postingComment, setPostingComment }) => {
 
   useEffect(() => {
     getLikeNumberForComment(comment.id).then((likeNumber) => {
-      setLike(likeNumber)
-    })
+      setLike(likeNumber);
+    });
     getDislikeNumberForComment(comment.id).then((dislikeNumber) => {
-      setDislike(dislikeNumber)
-    })
-    checkLikeStatus()
-   }, []);
+      setDislike(dislikeNumber);
+    });
+    checkLikeStatus();
+  }, []);
 
   const handleToggleReplies = () => setHideReplies(!hideReplies);
   const handleToggleReplyForm = () => setNewReply(!newReply);
@@ -90,22 +97,22 @@ const Comment = ({ comment, postingComment, setPostingComment }) => {
       <div className="w-full bg-white py-4 px-8 shadow-lg mb-2 border-l-4 border-add">
         <div className="mb-4">
           <Creator
-            openUserDialog= {true}
+            openUserDialog={true}
             avatarURL={comment.user.avatarUrl}
             name={comment.user.name}
             createdAt={comment.createdAt}
-            userId= {comment.userRef.id}
+            userId={comment.user.id}
           />
         </div>
         <p className="text-sm text-justify border-b border-gray-200 pb-4 mb-3">
           {comment.content.trim()}
         </p>
         <div className="flex justify-between items-center">
-          <LikeDislike 
-            like={like} 
-            dislike={dislike} 
-            isLiked={isLiked} 
-            isDisliked={isDisliked} 
+          <LikeDislike
+            like={like}
+            dislike={dislike}
+            isLiked={isLiked}
+            isDisliked={isDisliked}
             onLike={handleLike}
             onDislike={handleDislike}
           />
